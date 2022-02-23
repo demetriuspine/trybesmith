7997f7c67a7e5dc07b7deef0ebc08c881faef5ad
lucas <mr.pine97@gmail.com>
2022-02-23 17:20:36 -0300

    adiciona enum de status code

---
+++ src/enums/StatusCode.ts
@@ -0,0 +1,11 @@
+enum StatusCode {
+  OK = 200,
+  CREATED = 201, // ou CREATED,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  UNPROCESSABLE_ENTITY = 422,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export default StatusCode;
