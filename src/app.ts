36283100561f1945157218a861e9030de08371fa
lucas <mr.pine97@gmail.com>
2022-02-23 17:48:45 -0300

    usa a rota user com o path users e adiciona o middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/errorMiddleware';
+import userRouter from './router/user';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', userRouter);
+app.use(errorMiddleware);
 
 export default app;
