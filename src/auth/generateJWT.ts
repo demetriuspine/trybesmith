9dcc081c81f9e26ed90ab4403f36d4e76fc24b12
lucas <mr.pine97@gmail.com>
2022-02-23 17:32:48 -0300

    adiciona o generateJWT e verifyJWT

---
+++ src/auth/generateJWT.ts
@@ -0,0 +1,11 @@
+import { sign, verify } from 'jsonwebtoken';
+import { Token } from '../types/User';
+
+const secret = 'trybesmith';
+
+export const generateJWT = (data: Token) => {
+  const token: string = sign({ data }, secret);
+  return token;
+};
+
+export const verifyJWT = (token: string) => verify(token, secret);
