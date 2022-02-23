d6c928e1920181fc74b18193250e26eca005a7c7
lucas <mr.pine97@gmail.com>
2022-02-23 17:26:12 -0300

    adiciona o model user com o metodo createUser

---
+++ src/models/user.ts
@@ -0,0 +1,26 @@
+import { ResultSetHeader } from 'mysql2';
+import { User, UserID } from '../types/User';
+import connection from './connection';
+
+const createUser = async ({ username, password, classe, level }: User) => {
+  const query = `
+  INSERT INTO Trybesmith.Users (username, password, classe, level)
+  VALUES (?, ?, ?, ?)
+  `;
+
+  const values = [username, password, classe, level];
+
+  const [rows] = await connection.execute<ResultSetHeader>( // adaptado do vídeo do renato https://trybecourse.slack.com/archives/C023YHXAEGM/p1645562224495599?thread_ts=1645561923.462649&cid=C023YHXAEGM
+    query,
+    values,
+  );
+  
+  const { insertId: id } = rows; // desestruturando a prop insertId e renomeando para id
+  const user: UserID = { id, username, password, classe, level };
+
+  return user;
+};
+
+export default {
+  createUser,
+};
