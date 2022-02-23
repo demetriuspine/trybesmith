ef267509348121ea078fbeabc6236df2c5c877e6
lucas <mr.pine97@gmail.com>
2022-02-23 17:46:48 -0300

    adiciona o controller de user com o metodo createuser

---
+++ src/controllers/user.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import userService from '../services/user';
+import { User, UserID, userSchema } from '../types/User';
+import { generateJWT } from '../auth/generateJWT';
+import StatusCode from '../enums/StatusCode';
+
+const createUser = async (req: Request, res: Response) => {
+  const data: User = req.body;
+
+  userSchema.parse(data); // https://www.npmjs.com/package/zod#basic-usage
+  
+  const newUser: UserID = await userService.createUser(req.body);
+  const { id, username } = newUser;
+  const token = generateJWT({ id, username });
+
+  return res.status(StatusCode.CREATED).json({ token });
+};
+
+export default {
+  createUser,
+};
