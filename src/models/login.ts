7cd1c61e3c2f592f37719cd9e28b8e79b9610486
lucas <mr.pine97@gmail.com>
2022-02-25 14:06:36 -0300

    adiciona o model login

---
+++ src/models/login.ts
@@ -0,0 +1,18 @@
+import { RowDataPacket } from 'mysql2';
+import { Login, Token } from '../types/User';
+import connection from './connection';
+
+const login = async ({ username, password }: Login): Promise<Token[]> => {
+  const query = 'SELECT id, username FROM Trybesmith.Users WHERE username = ? AND password = ?'; // select retorna array de rowdatapacket
+  const values = [username, password];
+  
+  const [rows] = await connection.execute<RowDataPacket[]>(query, values);
+  
+  console.log(rows);
+  
+  return rows as Token[];
+};
+
+export default {
+  login,
+};
