c11dc6c15ebc200d5e7c9cb03ec085fdf937c43b
lucas <mr.pine97@gmail.com>
2022-02-25 20:07:09 -0300

    adiciona os tipos relacionados ao produto

---
+++ src/types/Product.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const productSchema = z.object({
+  name: z.string({
+    required_error: 'Name is required',
+    invalid_type_error: 'Name must be a string',
+  }).min(3, { message: 'Name must be longer than 2 characters' }),
+
+  amount: z.string({
+    required_error: 'Amount is required',
+    invalid_type_error: 'Amount must be a string',
+  }).min(3, { message: 'Amount must be longer than 2 characters' }),
+
+});
+
+export type Product = z.infer<typeof productSchema>;
+
+export type ProductObj = {
+  item: {
+    id: number,
+    name: string, 
+    amount: string
+  }
+};
