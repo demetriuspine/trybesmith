4bb4b1045d11251d826963b92e76c22cc281c4c4
lucas <mr.pine97@gmail.com>
2022-02-25 20:09:13 -0300

    trata o erro relacionado ao tipo JsonWebTokenError

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -4,6 +4,11 @@ import StatusCode from '../enums/StatusCode';
 
 const errorMiddleware: ErrorRequestHandler = async (err, _req, res, _next) => { // https://stackoverflow.com/questions/50218878/typescript-express-error-function
   console.log(`err: ${err}`);
+
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+
   if (err instanceof ZodError) { // https://github.com/colinhacks/zod/blob/master/ERROR_HANDLING.md
     const statusCode = err.issues[0]
       .message.endsWith('required') ? StatusCode.BAD_REQUEST : StatusCode.UNPROCESSABLE_ENTITY;
