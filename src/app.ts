d30a66b8d16326aaf677e6c9d261b018bafec1f5
lucas <mr.pine97@gmail.com>
2022-02-25 14:07:59 -0300

    usa o router login associado com a rota login

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import errorMiddleware from './middlewares/errorMiddleware';
 import userRouter from './router/user';
+import loginRouter from './router/login';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRouter);
-app.use(errorMiddleware);
+app.use('/login', loginRouter);
+app.use(errorMiddleware); // sempre vir depois das rotas
 
 export default app;
