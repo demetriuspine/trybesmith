bbf32a4cc84ab47c83a75ea906c6dfd39703acfc
lucas <mr.pine97@gmail.com>
2022-02-25 20:23:13 -0300

    usa a rota product

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import errorMiddleware from './middlewares/errorMiddleware';
 import userRouter from './router/user';
 import loginRouter from './router/login';
+import productsRouter from './router/products';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
+app.use('/products', productsRouter);
 app.use(errorMiddleware); // sempre vir depois das rotas
 
 export default app;
