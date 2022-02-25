664caae01e41b877b36fc79730bc7649372f4ab2
lucas <mr.pine97@gmail.com>
2022-02-25 14:07:02 -0300

    adiciona o controller login

---
+++ src/controllers/login.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import loginService from '../services/login';
+import { Login, loginSchema } from '../types/User';
+import { generateJWT } from '../auth/generateJWT';
+import StatusCode from '../enums/StatusCode';
+
+const login = async (req: Request, res: Response) => {
+  const user: Login = req.body;
+
+  loginSchema.parse(user); // https://www.npmjs.com/package/zod#basic-usage
+  
+  const loggedUser = await loginService.login(user);
+
+  if (!loggedUser) {
+    return res.status(401).json({ error: 'Username or password invalid' });
+  }
+
+  const { id, username } = loggedUser;
+  const token = generateJWT({ id, username });
+
+  return res.status(StatusCode.OK).json({ token });
+};
+
+export default {
+  login,
+};
