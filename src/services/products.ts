e8176cab22d4977b312dbbd01a83a180fc44225d
lucas <mr.pine97@gmail.com>
2022-02-26 10:25:01 -0300

    adiciona o service get product

--- src/services/products.ts
+++ src/services/products.ts
@@ -7,6 +7,13 @@ const addProduct = async (product: Product): Promise<ProductObj> => {
   return newProduct;
 };
 
+const getAllProducts = async () => {
+  const products = await productsModel.getAllProducts();
+
+  return products;
+};
+
 export default {
   addProduct,
+  getAllProducts,
 };
