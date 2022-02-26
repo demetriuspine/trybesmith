b2ced3567ba75390125effa52080455ab2f76460
lucas <mr.pine97@gmail.com>
2022-02-26 10:25:17 -0300

    adiciona o controller get product

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import 'express-async-errors';
 import { Product, ProductObj, productSchema } from '../types/Product';
 import productsService from '../services/products';
+import StatusCode from '../enums/StatusCode';
 
 const createProduct = async (req: Request, res: Response) => {
   const data: Product = req.body;
@@ -10,9 +11,16 @@ const createProduct = async (req: Request, res: Response) => {
 
   const newProduct: ProductObj = await productsService.addProduct(data);
 
-  return res.status(201).json(newProduct);
+  return res.status(StatusCode.CREATED).json(newProduct);
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+  const products = await productsService.getAllProducts();
+
+  return res.status(StatusCode.OK).json(products);
 };
 
 export default {
   createProduct,
+  getAllProducts,
 };
